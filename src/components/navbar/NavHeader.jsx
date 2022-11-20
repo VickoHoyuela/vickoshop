00401ae065b11e183dd20e88ecd0002706738bd4
Victor Hoyuela <victor.hoyuela@kantar.com>
2022-11-20 01:03:50 -0600

    Cards de los productos añadidos

---
+++ src/components/navbar/NavHeader.jsx
@@ -0,0 +1,13 @@
+import "./NavbarHeader.css"
+
+function NavHeader() {
+  return (
+    <div className="NavHeader">
+      <div className="btn_nav">Registrarse</div>
+      <div className="btn_nav">Iniciar sesión</div>
+      <div className="btn_nav">Carrito</div>
+    </div>
+  )
+}
+
+export default NavHeader
